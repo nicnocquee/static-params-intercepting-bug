b359cd3aa9aaefda1b72ee1c3ac7fb501cc65665
Nico Prananta <311343+nicnocquee@users.noreply.github.com>
2024-02-23 19:42:00 +0100

    add photos page

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -1,7 +1,24 @@
+import Link from "next/link";
+
 export default function Home(props: { params: { lng: string } }) {
+  let photos = Array.from({ length: 6 }, (_, i) => i + 1);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Current language: {props.params.lng}</div>
+
+      <section className="cards-container">
+        {photos.map((id) => (
+          <Link
+            className="card"
+            key={id}
+            href={`/${props.params.lng}/photos/${id}`}
+            passHref
+          >
+            {id}
+          </Link>
+        ))}
+      </section>
     </main>
   );
 }
